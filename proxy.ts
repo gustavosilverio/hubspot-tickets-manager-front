b5f077b71d646de2bd77050327045acd78517626
GustavoSilverio <gustavosk290905@gmail.com>
2026-04-26 20:14:37 -0300

    middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
 	return NextResponse.next()
 }
